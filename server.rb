266f0167f3c340889a8bc29478771e41d1d4d3f7
mandlerm <amaya.melissa@gmail.com>
2019-09-26 20:26:49 -0500

    simple server set up

--- server.rb
+++ server.rb
@@ -1,11 +1,6 @@
 require "socket"
 
-server = TCPServer.new("localhost", 3003)
-loop do
-  client = server.accept
-
-  request_line = client.gets
-  next if request_line =~ /favicon/
+def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(" ")
   path, params = path_and_params.split("?")
 
@@ -14,14 +9,40 @@ loop do
     hash[key] = value
   end
 
-  client.puts request_line
+  [http_method, path, params]
+end
+
+server = TCPServer.new("localhost", 3003)
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  # client.puts request_line
+  next if !request_line || request_line =~ /favicon/
+
+  http_method, path, params = parse_request(request_line)
+
+  client.puts "HTTP/1.1 200/OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
   client.puts http_method
   client.puts path
   client.puts params
+  client.puts "</pre>"
 
+  client.puts "<h1>Rolls!</h1>"
   rolls = params["rolls"].to_i
   sides = params["sides"].to_i
-  client.puts rand(6) + 1
 
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts "<p>",roll, "</p>"
+  end
+
+  client.puts "</body>"
+  client.puts "</html>"
   client.close
 end
