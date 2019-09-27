73900cdee359bb73e51e92661d2e2e5ec153fed2
mandlerm <amaya.melissa@gmail.com>
2019-09-26 20:57:37 -0500

    add and subtract links

--- counter.rb
+++ counter.rb
@@ -4,7 +4,7 @@ def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(" ")
   path, params = path_and_params.split("?")
 
-  params = params.split("&").each_with_object({}) do |pair, hash|
+  params = (params || "").split("&").each_with_object({}) do |pair, hash|
     key, value = pair.split("=")
     hash[key] = value
   end
@@ -33,14 +33,13 @@ loop do
   client.puts params
   client.puts "</pre>"
 
-  client.puts "<h1>Rolls!</h1>"
-  rolls = params["rolls"].to_i
-  sides = params["sides"].to_i
+  client.puts "<h1>Counter</h1>"
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
 
-  rolls.times do
-    roll = rand(sides) + 1
-    client.puts "<p>",roll, "</p>"
-  end
+  client.puts "<a href='?number=#{number + 1}'>Add 1</a>"
+  client.puts
+  client.puts "<a href='?number=#{number - 1}'>Subtract 1</a>"
 
   client.puts "</body>"
   client.puts "</html>"
