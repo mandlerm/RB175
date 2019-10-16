a7012361de3ab7a3f547ab020f50fa7c55f1fce8
mandlerm <amaya.melissa@gmail.com>
2019-10-16 08:33:57 -0500

    added helper for counter users and counter interests. displays in user view

--- users_and_interests.rb
+++ users_and_interests.rb
@@ -19,5 +19,19 @@ get "/:user" do
   @user_profile = @names[@name.to_sym] if @names.has_key?(@name.to_sym)
 
   erb :user
+end
+
+helpers do
+  def count_users
+    num_of_users = @names.keys.size
+  end
+
+  def count_interests
+    counter = 0
+    @names.each do |user|
+       counter += @names[user[0].to_sym][:interests].size
+    end
+    counter
+  end
 
 end
