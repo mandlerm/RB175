f1c08c208b57850037dc05a9bae86accfb3e8afa
mandlerm <amaya.melissa@gmail.com>
2019-10-15 21:23:32 -0500

    user route

--- users_and_interests.rb
+++ users_and_interests.rb
@@ -1,8 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
 require 'yaml'
+require 'pry'
+
 
 get "/" do
-  "Hello world"
+  @names = YAML.load_file("./users.yaml").keys
+
+  erb :names
+
+end
+
+get "/:user" do
+  @name = params[:user]
+
+  erb :user
 
 end
